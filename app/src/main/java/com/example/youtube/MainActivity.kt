60369f26a1e3d78c67691c16999bb97d8eaaefe9
bharath9812 <mandadhibharathsimham@gmail.com>
2023-06-30 13:08:11 +0530

    work done

--- app/src/main/java/com/example/youtube/MainActivity.kt
+++ app/src/main/java/com/example/youtube/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.youtube
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,10 +8,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.AccountBox
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.youtube.ui.theme.YoutubeTheme
@@ -19,24 +22,46 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.Abs
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             YoutubeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "Youtube",
+
+                                    //style = MaterialTheme.typography.h6,
+                                    color = Color.Red // Set the color to red
+                                )
+                            },
+                            navigationIcon = {
+                                Icon(
+                                    imageVector = Icons.Sharp.AccountBox,
+                                    contentDescription = "YouTube Icon"
+                                )
+                            }
+                        )
+                    }
                 ) {
-                    LazyColumn(modifier = Modifier.fillMaxSize()) {
-                        item {
-                            YoutubeScreen(videoId = "SYc922ntnKM")
-                            Spacer(modifier = Modifier.height(100.dp))
-                            YoutubeScreen(videoId = "PCp1BmME6QA")
-                            Spacer(modifier = Modifier.height(100.dp))
-                            YoutubeScreen(videoId = "C6VuSPPkTeY")
-                            Spacer(modifier = Modifier.height(100.dp))
-                            YoutubeScreen(videoId = "ntz2c2z54dg")
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = Color.Black
+                    ) {
+                        LazyColumn(modifier = Modifier.fillMaxSize()) {
+                            item {
+                                Spacer(modifier = Modifier.height(59.dp))
+                                YoutubeScreen(videoId = "SYc922ntnKM")
+                                Spacer(modifier = Modifier.height(60.dp))
+                                YoutubeScreen(videoId = "SqB0lUcqFbA")
+                                Spacer(modifier = Modifier.height(60.dp))
+                                YoutubeScreen(videoId = "O8gCqSmtkrM")
+                                Spacer(modifier = Modifier.height(60.dp))
+                                YoutubeScreen(videoId = "iU_iWa9LL_s")
+                            }
                         }
                     }
                 }
@@ -55,6 +80,7 @@ fun YoutubeScreen(
             object : AbstractYouTubePlayerListener() {
                 override fun onReady(youTubePlayer: YouTubePlayer) {
                     super.onReady(youTubePlayer)
+                    youTubePlayer.pause() // Pause the video by default
                     youTubePlayer.loadVideo(videoId, 0f)
                 }
             }
